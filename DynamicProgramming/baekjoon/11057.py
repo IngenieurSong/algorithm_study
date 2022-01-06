32f1e35440351e4b5519f5ebb3be3d4a08768497
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2022-01-07 03:22:30 +0900

    Create 11057.py
    백준 11057번 오르막수 문제에 대한 저의 코드입니다.

---
+++ DynamicProgramming/baekjoon/11057.py
@@ -0,0 +1,12 @@
+n = int(input())
+cache = [[0] * 10 for _ in range(n + 1)]
+
+for i in range(10):
+    cache[1][i] = 1
+
+for i in range(2, n + 1, 1):
+    for j in range(0, 10, 1):
+        for k in range(0, j + 1, 1):
+            cache[i][j] += cache[i - 1][k]
+
+print(sum(cache[n]) % 10007)
