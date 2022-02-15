8041994f73758cbd03d830289b8cb17653139652
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2022-02-16 03:20:45 +0900

    Create 15650.py
    백준 15650번 n과 m (2) 문제에 대한 저의 코드입니다.

---
+++ backtracking/baekjoon/15650.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+visited = [0] * (n + 1)
+result = [0]
+
+def bt(height):
+    global n, m
+
+    if(height == m):
+        print(' '.join(result[1:]))
+        return
+
+    for i in range(1, n + 1, 1):
+        if(visited[i] == 0 and int(result[-1]) < i):
+            visited[i] = 1
+            result.append(str(i))
+            bt(height + 1)
+            visited[i] = 0
+            result.pop()
+
+bt(0)
