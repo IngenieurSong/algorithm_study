267960b06e351a90e0872a81f843c59c92adfdc5
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2022-12-27 15:41:03 +0900

    Update 11055.py

--- DynamicProgramming/baekjoon/11055.py
+++ DynamicProgramming/baekjoon/11055.py
@@ -1,14 +1,13 @@
 n = int(input())
 board = [0] + list(map(int, input().split()))
-cache =  [0] * (n + 1)
+cache = [0] * (n + 1)
 cache[1] = board[1]
 
 for i in range(2, n + 1, 1):
-    temp_list = []
-    for j in range(i):
+    for j in range(1, i, 1):
         if(board[i] > board[j]):
-            temp_list.append(cache[j])
-    cache[i] = max(temp_list)
-    cache[i] += board[i]
+            cache[i] = max(cache[i], cache[j] + board[i])
+        else:
+            cache[i] = max(cache[i], board[i])
 
 print(max(cache))
