546ff766dff226ef5b69fb36b75bb40fad658dbd
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2022-12-28 15:31:38 +0900

    Update 2667.py

--- dfs&bfs/baekjoon/2667.py
+++ dfs&bfs/baekjoon/2667.py
@@ -1,39 +1,37 @@
-import sys
 from collections import deque
-
-def bfs(x, y, graph):
-    queue = deque([])
-    queue.append((x, y))
-    count = 0
-
-    while(queue):
-        x, y = queue.popleft()
-
-        if(graph[x][y] == 1):
-            if(x - 1 > -1):
-                queue.append((x - 1, y))
-            if(x + 1 < n):
-                queue.append((x + 1, y))
-            if(y - 1 > -1):
-                queue.append((x, y - 1))
-            if(y + 1 < n):
-                queue.append((x, y + 1))
-
-            graph[x][y] = 0
-            count += 1
-
-    return count
 
 n = int(input())
 graph = [list(map(int, input())) for _ in range(n)]
-count = []
+stride = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+def bfs(x, y, graph):
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for nx, ny in stride:
+            nx = x + nx
+            ny = y + ny
+
+            if(nx <= -1 or nx >= n or ny <= -1 or ny >= n):
+                continue
+            if(graph[nx][ny] == 0):
+                continue
+            graph[nx][ny] = 0
+            count += 1
+            queue.append((nx, ny))
+
+    return count
+
+result = []
 for i in range(n):
     for j in range(n):
-        if(graph[i][j] == 1):
-            count.append(bfs(i, j, graph))
+        if(graph[i][j]):
+            result.append(bfs(i, j, graph))
 
-print(len(count))
-count.sort()
-for i in count:
+print(len(result))
+for i in sorted(result):
     print(i)
