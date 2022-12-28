0b0068a580b818a65944b1d20e642240beac7bc2
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2022-12-28 15:09:52 +0900

    Update 2178.py

--- dfs&bfs/baekjoon/2178.py
+++ dfs&bfs/baekjoon/2178.py
@@ -1,26 +1,27 @@
 from collections import deque
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs(x, y, graph):
-    queue = deque([])
-    queue.append((x, y))
-
-    while(queue):
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if(0 <= nx < n and 0 <= ny < m):
-                if(graph[nx][ny] == 1):
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append((nx, ny))
-
 n, m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
 
-bfs(0, 0, graph)
-print(graph[n - 1][m - 1])
+def bfs(x, y, graph):
+    queue = deque([(x, y)])
+    stride = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+    while queue:
+        x, y = queue.popleft()
+
+        for nx, ny in stride:
+            nx = x + nx
+            ny = y + ny
+
+            if(nx <= -1 or nx >= n or ny <= -1 or ny >= m):
+                continue
+            if(graph[nx][ny] == 0):
+                continue
+            if(graph[nx][ny] == 1):
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n - 1][m - 1]
+
+print(bfs(0, 0, graph))
