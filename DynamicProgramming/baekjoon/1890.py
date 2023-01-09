49646a1b70673b0ec8be4e14638ddfb408a791b1
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-09 21:18:35 +0900

    Update 1890.py

--- DynamicProgramming/baekjoon/1890.py
+++ DynamicProgramming/baekjoon/1890.py
@@ -5,15 +5,15 @@ cache[0][0] = 1
 
 for i in range(n):
     for j in range(n):
-        if(i == n - 1 and j == n - 1):
-            break
-        if(cache[i][j] != 0):
-            vector = [(0, board[i][j]), (board[i][j], 0)]
-            for k in vector:
-                nx = i + k[0]
-                ny = j + k[1]
+        if(cache[i][j] == 0 or (i == n - 1 and j == n - 1)):
+            continue
+        vector = [(0, board[i][j]), (board[i][j], 0)]
 
-                if(nx < n and ny < n):
-                    cache[nx][ny] += cache[i][j]
+        for nx, ny in vector:
+            nx = i + nx
+            ny = j + ny
+
+            if(nx < n and ny < n):
+                cache[nx][ny] += cache[i][j]
 
 print(cache[n - 1][n - 1])
