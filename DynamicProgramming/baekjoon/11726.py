f0c2b8d704758c8b774f9b9c2eca350f9ce68454
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-09 15:49:53 +0900

    Update 11726.py

--- DynamicProgramming/baekjoon/11726.py
+++ DynamicProgramming/baekjoon/11726.py
@@ -1,11 +1,7 @@
 n = int(input())
-cache = [0] * (n + 1)
+cache = [1] * (n + 1)
 
-for i in range(3):
-    cache[i] = i
-if(n < 3):
-    print(cache[n])
-else:
-    for i in range(3, n + 1, 1):
-        cache[i] = cache[i - 1] + cache[i - 2]
-    print(cache[n])
+for i in range(2, n + 1, 1):
+    cache[i] = cache[i - 1] + cache[i - 2]
+
+print(cache[n] % 10007)
