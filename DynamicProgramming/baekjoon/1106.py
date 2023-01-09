6f212908e25b63db375201c2c5a7494504fd5598
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-09 15:06:10 +0900

    Update 1106.py

--- DynamicProgramming/baekjoon/1106.py
+++ DynamicProgramming/baekjoon/1106.py
@@ -1,17 +1,12 @@
-import sys
-input = sys.stdin.readline
-print = sys.stdout.write
+c, n = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+cache = [0] + [float("inf")] * c
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(m)]
-cache = [float("inf")] * (n + 1)
-cache[0] = 0
+for i in range(1, c + 1, 1):
+    for price, customer in board:
+        if(i - customer < 0):
+            cache[i] = min(cache[i], price)
+            continue
+        cache[i] = min(cache[i], cache[i - customer] + price)
 
-for i in range(1, n + 1, 1):
-    for j in board:
-        if(i - j[1] < 0):
-            cache[i] = min(cache[i], j[0])
-        else:
-            cache[i] = min(cache[i], cache[i - j[1]] + j[0])
-
-print(str(cache[n]))
+print(cache[c])
