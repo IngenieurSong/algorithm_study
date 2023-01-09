1a976d9de6f08dd9bb3b0d274e0a2c641cda7575
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-09 22:25:15 +0900

    Update 2156.py

--- DynamicProgramming/baekjoon/2156.py
+++ DynamicProgramming/baekjoon/2156.py
@@ -1,15 +1,12 @@
 n = int(input())
-board = [0] + list(int(input()) for _ in range(n))
+board = [0] + [int(input()) for _ in range(n)]
 cache = [0] * (n + 1)
+cache[1] = board[1]
 
-if(n > 2):
-    cache[1] = board[1]
-    cache[2] = board[1] + board[2]
-
-    for i in range(3, n + 1, 1):
-        cache[i] = max(cache[i - 1], cache[i - 3] + board[i - 1] + board[i], cache[i - 2] + board[i])
-
-    print(cache[n])
-
-else:
+if(n <= 2):
     print(sum(board))
+else:
+    for i in range(2, n + 1, 1):
+        cache[i] = max(cache[i - 1], cache[i - 2] + board[i], board[i] + board[i - 1] + cache[i - 3])
+
+    print(max(cache))
