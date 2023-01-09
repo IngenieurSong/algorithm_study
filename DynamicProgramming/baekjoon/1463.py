54ca625152082de90ecf9777f8e7bb041066eb40
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-09 19:46:52 +0900

    Update 1463.py

--- DynamicProgramming/baekjoon/1463.py
+++ DynamicProgramming/baekjoon/1463.py
@@ -1,11 +1,12 @@
 n = int(input())
-cache = [0] * (n + 1)
+cache = [float("inf")] * (n + 1)
+cache[1] = 0
 
 for i in range(2, n + 1, 1):
-    cache[i] = cache[i - 1] + 1
     if(i % 3 == 0):
         cache[i] = min(cache[i], cache[i // 3] + 1)
     if(i % 2 == 0):
         cache[i] = min(cache[i], cache[i // 2] + 1)
+    cache[i] = min(cache[i], cache[i - 1] + 1)
 
 print(cache[n])
