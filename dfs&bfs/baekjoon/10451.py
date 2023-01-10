fcb23ea09b2aa2c02a1cff2e825c833875ee345a
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-10 14:32:00 +0900

    Update 10451.py

--- dfs&bfs/baekjoon/10451.py
+++ dfs&bfs/baekjoon/10451.py
@@ -1,31 +1,33 @@
-import sys
 from collections import deque
 
 t = int(input())
 
+def bfs(start, graph, visited):
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+
+        for i in graph[node]:
+            if(i == start):
+                return 1
+            queue.append(i)
+            visited[i] = 1
+
+    return 0
+
 for _ in range(t):
     n = int(input())
-    graph = [[] for _ in range(n + 1)]
-    visited = [0 for i in range(n + 1)]
+    board = [0] + list(map(int, input().split()))
+    graph = [[] for _ in range(len(board))]
+    visited = [0] * len(board)
     count = 0
 
-    board = [0] + list(map(int, input().split()))
-    for i in range(1, n + 1, 1):
+    for i in range(1, len(board), 1):
         graph[i].append(board[i])
 
-    for i in range(1, n + 1, 1):
+    for i in range(1, len(board), 1):
         if(not visited[i]):
-            need_visited = deque([])
-            need_visited.append(i)
+            count += bfs(i, graph, visited)
 
-            while(need_visited):
-                node = need_visited.pop()
-
-                if(not visited[node]):
-                    visited[node] = 1
-                    need_visited.extend(graph[node])
-
-                else:
-                    count += 1
-                    break
     print(count)
