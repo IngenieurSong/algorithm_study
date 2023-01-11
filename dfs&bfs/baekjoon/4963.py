99c6a56a1b2f4ff23a6f6e560f1da26d52bda912
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-11 17:47:57 +0900

    Update 4963.py

--- dfs&bfs/baekjoon/4963.py
+++ dfs&bfs/baekjoon/4963.py
@@ -1,38 +1,38 @@
 from collections import deque
 
-dx = [1, -1, 0, 0, 1, -1, 1, -1]
-dy = [0, 0, -1, 1, -1, 1, 1, -1]
+vector = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, -1), (-1, 1), (1, 1), (-1, -1)]
 
-def bfs(x, y, graph, h, w):
-    queue = deque([])
-    queue.append((x, y))
-    graph[x][y] = 0
+def bfs(x, y, graph, visited, n, m):
+    queue = deque([(x, y)])
+    visited[x][y] = 1
 
-    while(queue):
+    while queue:
         x, y = queue.popleft()
 
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if(0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1):
-                graph[nx][ny] = 0
+        for nx, ny in vector:
+            nx = x + nx
+            ny = y + ny
+
+            if(nx < 0 or nx >= n or ny < 0 or ny >= m):
+                continue
+            if(graph[nx][ny] == 1 and not visited[nx][ny]):
+                visited[nx][ny] = 1
                 queue.append((nx, ny))
 
 while(True):
-    w, h = map(int, input().split())
-    count = 0
+    m, n = map(int, input().split())
 
-    if(w == 0 or h == 0):
+    if(n == 0):
         break
 
-    graph = []
-    for _ in range(h):
-        graph.append(list(map(int, input().split())))
+    board = [list(map(int, input().split())) for _ in range(n)]
+    count = 0
+    visited = [[0] * m for _ in range(n)]
 
-    for i in range(h):
-        for j in range(w):
-            if(graph[i][j] == 1):
-                bfs(i, j, graph, h, w)
+    for i in range(n):
+        for j in range(m):
+            if(board[i][j] == 1 and not visited[i][j]):
+                bfs(i, j, board, visited, n, m)
                 count += 1
 
     print(count)
