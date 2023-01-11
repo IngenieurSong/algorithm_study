000409d131478671a20b4ee9428f4c9c6766c4aa
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-11 18:50:10 +0900

    Update 7562.py

--- dfs&bfs/baekjoon/7562.py
+++ dfs&bfs/baekjoon/7562.py
@@ -1,32 +1,42 @@
 from collections import deque
 
 t = int(input())
-vector = [(-2, 1), (-1, 2), (1, 2), (2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+vector = [(2, 1), (2, -1), (1, 2), (1, -2), (-2, -1), (-2, 1), (-1, -2), (-1, 2)]
 
-def bfs(x, y, graph, l):
-    queue = deque([])
-    queue.append((x, y))
+def bfs(x_s, y_s, x_e, y_e, graph, visited):
+    queue = deque([(x_s, y_s)])
+    visited[x_s][y_s] = 1
 
-    while(queue):
+    while queue:
         x, y = queue.popleft()
 
-        for i in vector:
-            nx = x + i[0]
-            ny = y + i[1]
+        for nx, ny in vector:
+            nx = x + nx
+            ny = y + ny
 
-            if(0 <= nx < l and 0 <= ny < l and graph[nx][ny] == 0):
+            if(nx == x_e and ny == y_e):
+                graph[nx][ny] = graph[x][y] + 1
+                return
+
+            if(nx < 0 or nx >= l or ny < 0 or ny >= l):
+                continue
+            if(graph[nx][ny] == 0 and not visited[nx][ny]):
+                visited[nx][ny] = 1
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
 
 for _ in range(t):
     l = int(input())
-    graph = [[0] * l for _ in range(l)]
-    s1, s2 = map(int, input().split())
-    t1, t2 = map(int, input().split())
+    x_s, y_s = map(int, input().split())
+    x_e, y_e = map(int, input().split())
 
-    if(s1 == t1 and s2 == t2):
+    if(x_s == x_e and y_s == y_e):
         print(0)
         continue
 
-    bfs(s1, s2, graph, l)
-    print(graph[t1][t2])
+    visited = [[0] * l for _ in range(l)]
+    board = [[0] * l for _ in range(l)]
+    board[x_s][y_s] = 1
+
+    bfs(x_s, y_s, x_e, y_e, board, visited)
+    print(board[x_e][y_e] - 1)
