8457ae34e8a3ae34417a9b2743996e13acf37410
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-11 14:16:56 +0900

    Update 2606.py

--- dfs&bfs/baekjoon/2606.py
+++ dfs&bfs/baekjoon/2606.py
@@ -1,29 +1,29 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(5000)
+
+def find_parent(parent, x):
+    if(parent[x] != x):
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if(a < b):
+        parent[b] = a
+    else:
+        parent[a] = b
 
 n = int(input())
 m = int(input())
-visited = [0] * (n + 1)
-graph = [[] for _ in range(n + 1)]
+parent = [i for i in range(n + 1)]
 
 for _ in range(m):
-    k, v = map(int, input().split())
-    graph[k].append(v)
-    graph[v].append(k)
+    u, v = map(int, input().split())
+    union_parent(parent, u, v)
 
-def bfs(start, graph, visited):
-    queue = deque([])
-    count = 0
-    queue.append(start)
-    visited[start] = 1
+for i in range(1, n + 1, 1):
+    find_parent(parent, i)
 
-    while(queue):
-        node = queue.popleft()
-
-        for i in graph[node]:
-            if(not visited[i]):
-                visited[i] = 1
-                count += 1
-                queue.append(i)
-    return count
-
-print(bfs(1, graph, visited))
+print(parent.count(1) - 1)
