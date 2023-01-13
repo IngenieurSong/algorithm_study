21dd914919d1682a4f4389c31d5b601e82cfa471
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-13 15:18:00 +0900

    Update 1758.py

--- greedy/baekjoon/1758.py
+++ greedy/baekjoon/1758.py
@@ -1,11 +1,9 @@
 n = int(input())
 board = [int(input()) for _ in range(n)]
-board.sort(reverse = True)
-board = [0] + board
-answer = 0
+board = sorted(board, reverse = True)
+result = 0
 
-for i in range(1, n + 1, 1):
-    if(board[i] - (i - 1) > 0):
-        answer += board[i] - (i - 1)
+for i in range(n):
+    result += max(board[i] - i, 0)
 
-print(answer)
+print(result)
