3d8a064d5f9c5e8678bc3253e032a1d4ba6817c7
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-13 14:35:50 +0900

    Update 2217.py

--- greedy/baekjoon/2217.py
+++ greedy/baekjoon/2217.py
@@ -1,10 +1,9 @@
 n = int(input())
 board = [int(input()) for _ in range(n)]
 board.sort()
-max_sum = 0
+weight = 0
 
 for i in range(n):
-    if(max_sum < board[i] * (n - i)):
-        max_sum = board[i] * (n - i)
+    weight = max(weight, board[i] * (n - i))
 
-print(max_sum)
+print(weight)
