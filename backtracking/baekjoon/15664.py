7c51309001f2a3537bd9d20c61afcc39c299a8c9
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-15 18:35:58 +0900

    Update 15664.py

--- backtracking/baekjoon/15664.py
+++ backtracking/baekjoon/15664.py
@@ -1,27 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def bt(height):
-    if(height == m):
-        string = ' '.join(result[1:])
+n, m = map(int, input().split())
+result = ['0'] + []
+visited = [0] * (n + 1)
+board = sorted(list(map(int, input().split())))
+result_dict = {}
 
-        if string not in dictionary:
-            dictionary[string] = 1
-            print(string)
+def bt(height):
+    global n, m
+
+    if(height == m):
+        if(result_dict.get(' '.join(result)) == None):
+            result_dict[' '.join(result)] = 1
+            print(' '.join(result[1 : m + 1]))
         return
 
     for i in range(n):
-        if(visited[i] == 0 and int(result[-1]) <= board[i]):
-            result.append(str(board[i]))
-            visited[i] = 1
-            bt(height + 1)
-            result.pop()
-            visited[i] = 0
+            if(not visited[i] and int(result[height]) <= board[i]):
+                visited[i] = 1
+                result.append(str(board[i]))
+                bt(height + 1)
+                visited[i] = 0
+                result.pop()
 
-n, m = map(int, input().split())
-board = list(map(int, input().split()))
-board.sort()
-dictionary = {}
-result = [0]
-visited = [0] * n
 bt(0)
