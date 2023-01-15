58bd23cb3252bc12b0f41e9ff09ce45764a9f265
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-15 18:38:48 +0900

    Update 15665.py

--- backtracking/baekjoon/15665.py
+++ backtracking/baekjoon/15665.py
@@ -1,24 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def bt(height):
-    if(height == m):
-        string = ' '.join(result[1:])
+n, m = map(int, input().split())
+result = []
+board = sorted(list(map(int, input().split())))
+result_dict = {}
 
-        if string not in dictionary:
-            dictionary[string] = 1
-            print(string)
+def bt(height):
+    global n, m
+
+    if(height == m):
+        if(result_dict.get(' '.join(result)) == None):
+            result_dict[' '.join(result)] = 1
+            print(' '.join(result))
         return
 
     for i in range(n):
-        if(int(result[-1]) <= board[i]):
-            result.append(str(board[i]))
-            bt(height + 1)
-            result.pop()
+        result.append(str(board[i]))
+        bt(height + 1)
+        result.pop()
 
-n, m = map(int, input().split())
-board = list(map(int, input().split()))
-board.sort()
-dictionary = {}
-result = [0]
 bt(0)
