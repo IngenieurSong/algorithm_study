63921fac9d126ba5fb77b58a9ca9a5a85419b3e3
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-15 18:08:45 +0900

    Update 15656.py

--- backtracking/baekjoon/15656.py
+++ backtracking/baekjoon/15656.py
@@ -2,18 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-board = list(map(int, input().split()))
-board.sort()
 result = []
+board = sorted(list(map(int, input().split())))
 
 def bt(height):
+    global n, m
+
     if(height == m):
         print(' '.join(result))
         return
 
-    for i in board:
-        result.append(str(i))
-        bt(height + 1)
-        result.pop()
- 
+    for i in range(n):
+            result.append(str(board[i]))
+            bt(height + 1)
+            result.pop()
+
 bt(0)
