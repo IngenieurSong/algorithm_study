9c769a2bb6d935be5134b9d0ddeaaacbb160fc2b
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-15 17:58:25 +0900

    Update 15652.py

--- backtracking/baekjoon/15652.py
+++ backtracking/baekjoon/15652.py
@@ -2,17 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-result = [0]
+visited = [0] * (n + 1)
+result = ['0'] + []
 
 def bt(height):
+    global n, m
+
     if(height == m):
-        print(' '.join(result[1:]))
+        print(' '.join(result[1 : m + 1]))
         return
 
     for i in range(1, n + 1, 1):
-        if(int(result[-1]) <= i):
-            result.append(str(i))
-            bt(height + 1)
-            result.pop()
+            if(int(result[height]) <= i):
+                result.append(str(i))
+                bt(height + 1)
+                result.pop()
 
 bt(0)
