62efa8435374401d089e4afa3da87b49e6fd794b
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-01-26 15:39:05 +0900

    Update 2294.py

--- DynamicProgramming/baekjoon/2294.py
+++ DynamicProgramming/baekjoon/2294.py
@@ -1,14 +1,10 @@
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 board = [int(input()) for _ in range(n)]
-cache = [float("inf")] * (m + 1)
-cache[0] = 0
+cache = [0] + [float("inf")] * k
 
-for i in range(1, m + 1, 1):
+for i in range(1, k + 1, 1):
     for j in board:
         if(i - j >= 0):
             cache[i] = min(cache[i], cache[i - j] + 1)
 
-if(cache[m] == float("inf")):
-    print(-1)
-else:
-    print(cache[m])
+print(cache[k] if cache[k] != float("inf") else -1)
