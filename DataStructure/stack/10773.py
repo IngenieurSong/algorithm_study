5bd5640c2dfb1a578e2adf358fadc7a4565255ff
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-03-18 21:49:15 +0900

    Create 10773.py

---
+++ DataStructure/stack/10773.py
@@ -0,0 +1,12 @@
+k = int(input())
+stack = []
+
+for _ in range(k):
+    number = int(input())
+
+    if(number == 0):
+        stack.pop()
+    else:
+        stack.append(number)
+
+print(sum(stack))
