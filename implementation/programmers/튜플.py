5084fdf0cdc13c2a0c3f02d93b11326be089fa66
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-04-04 14:29:28 +0900

    Create 튜플.py

---
+++ implementation/programmers/튜플.py
@@ -0,0 +1,20 @@
+def strToList(s):
+    board = []
+    s = s[2:-2].split("},{")
+
+    for i in s:
+        board.append(list(map(int, i.split(','))))
+
+    return board
+
+def solution(s):
+    answer = []
+    board = strToList(s)
+    board.sort(key = lambda x : len(x))
+
+    for target in board:
+        for t in target:
+            if(t not in answer):
+                answer.append(t)
+
+    return answer
