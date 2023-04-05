776fcf19de6f6b875f51c4413b4c41e3e4cc1355
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2023-04-05 13:00:05 +0900

    Update 연속부분수열합의개수.py

--- DataStructure/set/연속부분수열합의개수.py
+++ DataStructure/set/연속부분수열합의개수.py
@@ -8,3 +8,17 @@ def solution(elements):
             sum_list.add(sum(elements[j : j  + i]))
 
     return len(sum_list)
+
+# 나머지를 이용한 풀이
+def solution(elements):
+    sum_list = set()
+
+    for i in range(len(elements)):
+        sum_list.add(elements[i])
+        temp_sum = elements[i]
+
+        for j in range(i + 1, i + len(elements), 1):
+            temp_sum += elements[j % len(elements)]
+            sum_list.add(temp_sum)
+
+    return len(sum_list)
