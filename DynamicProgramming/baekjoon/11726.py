b1a641952f0374fdff16dd0c8ca93d6b74c6a0d9
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2024-08-12 23:03:18 +0900

    Update 11726.py

--- DynamicProgramming/baekjoon/11726.py
+++ DynamicProgramming/baekjoon/11726.py
@@ -2,6 +2,6 @@ n = int(input())
 cache = [1] * (n + 1)
 
 for i in range(2, n + 1, 1):
-    cache[i] = cache[i - 1] + cache[i - 2]
+    cache[i] = cache[i - 1] + cache[i - 2] # 가로 타일 두개를 쌓거나 세로 타일을 하나 쌓기
 
 print(cache[n] % 10007)
