0783050e070197c88844ba5fdcafc604cda02ee8
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2024-08-13 18:30:22 +0900

    Update 11727.py

--- DynamicProgramming/baekjoon/11727.py
+++ DynamicProgramming/baekjoon/11727.py
@@ -2,6 +2,6 @@ n = int(input())
 cache = [1] * (n + 1)
 
 for i in range(2, n + 1, 1):
-    cache[i] = cache[i - 1] + cache[i - 2] * 2
+    cache[i] = cache[i - 1] + cache[i - 2] * 2 # 2x2 조건 추가
 
 print(cache[n] % 10007)
