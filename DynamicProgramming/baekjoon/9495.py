31a685c518a4cc350da9cf1e43e255107d487bdd
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2024-08-21 19:51:12 +0900

    Update 9495.py

--- DynamicProgramming/baekjoon/9495.py
+++ DynamicProgramming/baekjoon/9495.py
@@ -1,10 +1,17 @@
 t = int(input())
+
 for _ in range(t):
     n = int(input())
-    cache = [[0] + list(map(int, input().split())) for _ in range(2)]
+    sticker = [list(map(int, input().split())) for _ in range(2)]
+    cache = [[0] * n for _ in range(2)]
+    cache[0][0] = sticker[0][0]
+    cache[1][0] = sticker[1][0]
 
-    for i in range(2, n + 1, 1):
-        cache[0][i] += max(cache[1][i - 2], cache[1][i - 1])
-        cache[1][i] += max(cache[0][i - 2], cache[0][i - 1])
+    for i in range(1, n, 1):
+        for j in range(2):
+            if(j == 0):
+                cache[j][i] = max(cache[j][i - 1], cache[j + 1][i - 1] + sticker[j][i])
+            else:
+                cache[j][i] = max(cache[j][i - 1], cache[j - 1][i - 1] + sticker[j][i])
 
-    print(max(cache[0][n], cache[1][n]))
+    print(max(cache[0][n - 1], cache[1][n - 1]))
