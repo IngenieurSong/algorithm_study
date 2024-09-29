6cba52b793f078fa22ce7d2e904568f2a3ac4a11
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2024-09-29 18:25:37 +0900

    Update 2606.py

--- dfs&bfs/baekjoon/2606.py
+++ dfs&bfs/baekjoon/2606.py
@@ -1,29 +1,32 @@
-import sys
-sys.setrecursionlimit(5000)
+n = int(input())
+m = int(input())
+parent = [i for i in range(n + 1)]
 
 def find_parent(parent, x):
-    if(parent[x] != x):
+    if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
+
     return parent[x]
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
 
-    if(a < b):
+    if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
-n = int(input())
-m = int(input())
-parent = [i for i in range(n + 1)]
-
 for _ in range(m):
-    u, v = map(int, input().split())
-    union_parent(parent, u, v)
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
 
-for i in range(1, n + 1, 1):
-    find_parent(parent, i)
+root_of_1 = find_parent(parent, 1)
+infected_computers = 0
 
-print(parent.count(1) - 1)
+# 각 컴퓨터가 1번 컴퓨터와 같은 부모를 가지는지 확인
+for i in range(2, n + 1):  # 1번 컴퓨터는 제외하고 카운트
+    if find_parent(parent, i) == root_of_1:
+        infected_computers += 1
+
+print(infected_computers)
