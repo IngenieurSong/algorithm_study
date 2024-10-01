04badd5f2f5e63937fe0df93db644eedcb669310
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2024-10-01 18:52:43 +0900

    Update 15654.py

--- backtracking/baekjoon/15654.py
+++ backtracking/baekjoon/15654.py
@@ -1,6 +1,5 @@
 n, m = map(int, input().split())
 result = []
-visited = [0] * n
 board = sorted(list(map(int, input().split())))
 
 def bt(height):
@@ -11,11 +10,8 @@ def bt(height):
         return
 
     for idx, i in enumerate(board):
-        if not visited[idx]:
-            visited[idx] = 1
-            result.append(str(i))
-            bt(height + 1)
-            visited[idx] = 0
-            result.pop()
+        result.append(str(i))
+        bt(height + 1)
+        result.pop()
 
 bt(0)
