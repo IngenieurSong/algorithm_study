ac59529ed6c18b811af587d8f42efe359cbbeef4
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2025-02-22 16:59:10 +0900

    Update 2193.py

--- DynamicProgramming/baekjoon/2193.py
+++ DynamicProgramming/baekjoon/2193.py
@@ -1,21 +1,9 @@
 n = int(input())
-cache = [0] * (n + 1)
-
-cache[1] = 1
-for i in range(2, n + 1, 1):
-    cache[i] = cache[i - 2] + cache[i - 1]
-
-print(cache[n])
-
-# 2차원 dp 풀이
-n = int(input())
-cache = [[0] * (n + 1) for _ in range(2)]
+cache = [[0] * 2 for _ in range(n + 1)]
 cache[1][1] = 1
 
-for column in range(2, n + 1, 1):
-    for row in range(2):
-        if(row == 0):
-            cache[row][column] = cache[row][column - 1] + cache[row + 1][column - 1]
-        else:
-            cache[row][column] = cache[row - 1][column - 1]
-print(cache[0][n] + cache[1][n])
+for i in range(2, n + 1, 1):
+    cache[i][0] = cache[i - 1][0] + cache[i - 1][1]
+    cache[i][1] = cache[i - 1][0]
+
+print(sum(cache[n]))
