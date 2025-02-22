fefd576cc7b58a5e63366b24fcdfd557e471a522
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2025-02-22 18:54:52 +0900

    Update 1912.py

--- DynamicProgramming/baekjoon/1912.py
+++ DynamicProgramming/baekjoon/1912.py
@@ -1,10 +1,9 @@
 n = int(input())
-# 인덱스 맞추려고 넣는게 계산에 포함됨
 board = list(map(int, input().split()))
 cache = [0] * n
 cache[0] = board[0]
 
 for i in range(1, n, 1):
-    cache[i] = max(cache[i - 1] + board[i], board[i])
+    cache[i] = max(board[i], cache[i - 1] + board[i])
 
 print(max(cache))
