9b02a557893371551d0799f052aef558eb4cd319
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2025-02-22 17:48:11 +0900

    Update 2156.py

--- DynamicProgramming/baekjoon/2156.py
+++ DynamicProgramming/baekjoon/2156.py
@@ -3,10 +3,10 @@ board = [0] + [int(input()) for _ in range(n)]
 cache = [0] * (n + 1)
 cache[1] = board[1]
 
-if(n > 1):
+if (n > 1):
     cache[2] = board[1] + board[2]
 
 for i in range(3, n + 1, 1):
-    cache[i] = max(cache[i - 1], cache[i - 2] + board[i], cache[i - 3] + board[i - 1] + board[i])
+    cache[i] = max(cache[i - 2] + board[i], cache[i - 1], cache[i - 3] + board[i - 1] + board[i])
 
 print(max(cache))
