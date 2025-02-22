0a6b70c3dbb94084f656e762a611470a41fbe881
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2025-02-22 16:45:43 +0900

    Update 11057.py

--- DynamicProgramming/baekjoon/11057.py
+++ DynamicProgramming/baekjoon/11057.py
@@ -1,8 +1,11 @@
 n = int(input())
-cache = [[1] * 10 for _ in range(n + 1)]
+cache = [[0] * 10 for _ in range(n + 1)]
+
+for i in range(10):
+    cache[1][i] = 1
 
 for i in range(2, n + 1, 1):
-    for j in range(1, 10, 1):
-        cache[i][j] = cache[i][j - 1] + cache[i - 1][j]
+    for j in range(10):
+        cache[i][j] = cache[i - 1][j] + cache[i][j - 1]
 
 print(sum(cache[n]) % 10007)
